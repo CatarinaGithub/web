1a84b0f81ae1474585fde72145d718ad6f22f942
Catarina Figueiredo <catarina.figueiredo3@vodafone.com>
2025-04-09 15:59:02 +0100

    Attributes with generics

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,9 +1,11 @@
+import { UserProps } from './User';
+
 export class Attributes<T extends object> {
   constructor(private data: T) {}
 
-  get = <K extends keyof T>(key: K): T[K] => {
+  get<K extends keyof T>(key: K): T[K] {
     return this.data[key];
-  };
+  }
 
   set(update: T): void {
     Object.assign(this.data, update);
@@ -13,3 +15,16 @@ export class Attributes<T extends object> {
     return this.data;
   }
 }
+
+// sample usage
+// Assuming UserProps is defined in User.ts
+// and has properties id, name, and age
+const attrs = new Attributes<UserProps>({
+  id: 1,
+  name: 'Cat',
+  age: 38,
+});
+
+const name = attrs.get('name'); // name is of type string
+const age = attrs.get('age'); // age is of type number
+const id = attrs.get('id'); // id is of type number | undefined
